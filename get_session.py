bb2e668d0f822f6abb5a393e263d1f12d295513e
akhil333ask <akhilskumar65046@gmail.com>
2026-07-15 20:28:17 +0530

    Delete get_session.py

--- get_session.py
+++
@@ -1,9 +0,0 @@
-from telethon.sync import TelegramClient
-from telethon.sessions import StringSession
-
-API_ID = 34989469
-API_HASH = '3d8e9f2619d7d993d1f943181eb4e8c3'
-
-with TelegramClient(StringSession(), API_ID, API_HASH) as client:
-    print("\n✅ YOUR STRING SESSION IS BELOW (KEEP IT SECRET):\n")
-    print(client.session.save())
